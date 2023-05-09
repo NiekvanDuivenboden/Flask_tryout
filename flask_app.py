c0f36eea03b5c9210c8266e32fed10ec095c4448
Niek van Duivenboden <niek.van.duivenboden@hotmail.com>
2023-05-09 15:45:36 +0200

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -3,4 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return"<p>Hello world!</p>"
+    return"<p>Hello world! This is my second flask website. </p>"
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
